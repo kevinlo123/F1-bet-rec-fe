3355ebf999ba1bac511b97a902b92a67d5560c8d
Kevin Lopez <lopezkevin175@gmail.com>
2024-06-26 15:05:40 -0400

    wrapping up constructors page FE

--- src/pages/constructor-standings.js
+++ src/pages/constructor-standings.js
@@ -1,23 +1,45 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { SectionTitleOne } from '../common/elements/sectionTitle/SectionTitle';
 import FooterOne from "../common/elements/footer/FooterOne";
 import HeaderFour from "../common/elements/header/HeaderFour";
 import { getAllPosts } from '../../lib/api';
 import HeadTitle from "../common/elements/head/HeadTitle";
+import constructorData from '../data/constructors/constructor-standings.json'
 
 const ConstructorStandings = ({allPosts}) => {
     return (
         <>
             <HeadTitle pageTitle="Constructor Standings" />
             <HeaderFour postData={allPosts} pClass="header-light header-sticky header-with-shadow" />
-            <div className="bg-color-grey">
+            <div className="bg-color-grey constructor-standings-wrapper axil-section-gap">
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-12">
                             <div className="inner">
-                                Constructor standings
+                                <h1 className="section-title mb--20">2024 Formula 1 Team Standings</h1>
                             </div>
                         </div>
+                    </div>
+
+                    <div className="col-lg-12 constructor-standings-container mt--20">
+                        {constructorData.standings.map((data) => ( 
+                            <div className="inner constructor-container" key={data.position}>
+                                <div className="constructor-teamColor" style={{backgroundColor: `${data.teamColor}`}}></div>
+                                <span className="constructor-position">P{data.position}</span>
+                                <div className="constructor-image">
+                                    <Image
+                                        src={data.logoImg}
+                                        height={40}
+                                        width={data.customImageWidth}
+                                        priority={true}
+                                    />
+                                </div>
+                                <span className="constructor">{data.team}</span>
+                                <span className="constructor-drivers">{data.drivers}</span>
+                                <span>{data.points}pts</span>                                
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
